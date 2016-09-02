e97e332cf65f4d7e74f08ef9605da09bf449de61
Matt Aadland <aadland6@users.noreply.github.com>
2016-09-02 07:36:15 -0400

    added wikipedia crawler

--- wikipedia_location_search.py
+++ wikipedia_location_search.py
@@ -10,14 +10,14 @@ import csv
 import requests 
 import json 
 
-def geo_search(lat,lon,radius):
+def geo_search(lat,lon,radius,language="en"):
     if radius > 10:
         print "Radius cannot be more than 10 km. Setting to 10"
         radius = 10
     radius = radius * 1000
-    url = ("https://en.wikipedia.org/w/api.php?action=query&list=geosearch"
-    "&gscoord={0}%7C{1}"
-    "&gsradius={2}&gslimit=500&format=json").format(lat,lon,radius)
+    url = ("https://{0}.wikipedia.org/w/api.php?action=query&list=geosearch"
+    "&gscoord={1}%7C{2}"
+    "&gsradius={3}&gslimit=500&format=json").format(language,lat,lon,radius)
     response = requests.get(url)
     if response.status_code == 200:
         search_results = json.loads(response.content)
@@ -34,26 +34,32 @@ def page_views(title,start,end):
         search_results = json.loads(response.content)
         return search_results
     else:
-        return None 
+        return [None,title] 
  
 def format_view(view):
-    items = view['items']
-    view_count = [item['views'] for item in items]
-    time_stamp = [item['timestamp'] for item in items]
-    return [view_count,time_stamp]
+    try:
+        items = view['items']
+        view_count = [item['views'] for item in items]
+        time_stamp = [item['timestamp'] for item in items]
+        return [view_count,time_stamp]
+    except:
+        return None
 
-def format_table(coords,scores,raw_values,timeseries):
+def format_table(title,coords,scores,raw_values,timeseries):
     output_list = []
     for i in range(len(scores)):
         date = str(timeseries[i])
         date_string = "{0}-{1}-{2}".format(date[0:4],date[4:6],date[6:8])
-        output_list.append((coords[0],coords[1],scores[i],raw_values[i],date_string))
+        output_list.append((title,coords[0],coords[1],scores[i],raw_values[i],date_string))
     return output_list
 
 def csv_output(rows,filename):
     with open(filename, "wb") as f:
         writer = csv.writer(f)
-        writer.writerows(rows)
+        for row in rows:
+            row = list(row)
+            row[0] = row[0].encode('utf-8')
+            writer.writerow(row)
 
 def discretize(value):
     if value < 1:
@@ -70,39 +76,50 @@ def discretize(value):
         return 1
     
 
-def bundle_places(pairs,views):
+def bundle_places(titles,pairs,views):
     rows = []
     for i in range(len(pairs)):
-        discrete = [discretize(x) for x in stats.zcore(views[i][0])]
-        current_row = format_table(pairs[i],discrete,views[i][0],views[i][1])
+        discrete = [discretize(x) for x in stats.zscore(views[i][0])]
+        current_row = format_table(titles[i],pairs[i],discrete,views[i][0],views[i][1])
         rows += current_row
     return rows 
 
-results = geo_search(50.9010, 4.4856,1)
-pairs = [(x['lat'],x['lon']) for x in results]
-formatted_titles = [x["title"].replace(" ","_") for x in results] 
-views = [page_views(x.encode('utf8'),20150101,20160101) for x in formatted_titles]
-formatted_views = [format_view(x) for x in views]
-rows = bundle_places(pairs,formatted_views)
+
+def wrapper(coords,radius,language,dates,title):
+    results = geo_search(coords[0],coords[1],radius,language)
+    # format titles 
+    formatted_titles = [x["title"].replace(" ","_") for x in results] 
+    # get views
+    views = [page_views(x.encode('utf8'),dates[0],dates[1]) for x in formatted_titles]
+    # extract titles without information available 
+    broken_titles = [x[1].decode('utf8').replace("_"," ") for x in views if type(x) == list]
+    filtered_views = [x for x in views if type(x) == dict]
+    # get geo coords for remaining views 
+    pairs = [(x['lat'],x['lon']) for x in results if x['title'] not in broken_titles]
+    # format the views 
+    formatted_views = [format_view(x) for x in filtered_views]
+    # buindle the data into csv rows 
+    return bundle_places(formatted_titles,pairs,formatted_views)
+
+# csv_output(rows,"{0}.csv".format(title))
+
+    
+coords = [59.917197,30.277542]
+language = "en"
+dates = [20150101,20160721]
+radius = 3
+title = "Astrakhan Shipbuilding Production Association JSC".lower()
+title = title.replace(" ","_")
+rows = wrapper(coords,radius,language,dates,title)
 
 
-#### Airport ####
-brussels_airport = page_views("Brussels_Airport",2015010100,2016032100)
-brussels_airport_view = format_view(brussels_airport)
-z_scores = stats.zscore(brussels_airport_view[0])
-z_scores = list(z_scores)
-airport_discret = [discretize(x) for x in z_scores]
-coords = (50.9010, 4.4856)
-rows = format_table(coords,airport_discret ,brussels_airport_view[0],brussels_airport_view[1])
-csv_output(rows,"brussels_airport.csv")
-
-#### Malebeek #### 
-metro_stations = page_views("List_of_Brussels_Metro_stations",2015010100,2016032100)
-metro_stations_view = format_view(metro_stations)
-metro_z_scores = stats.zscore(metro_stations_view[0])
-metro_discret = [discretize(x) for x in metro_z_scores]
-metro_coords = (50.843889, 4.376667)
-metro_rows = format_table(metro_coords,metro_discret,metro_stations_view[0],metro_stations_view[1])
-
-all_rows = rows + metro_rows
-csv_output(all_rows,"metro_and_airpot.csv")
+#
+#path = "C:/Users/aadlandma/Desktop/prisons_wikipedia/ftm.csv"
+#with open(path, 'rb') as f:
+#    reader = csv.reader(f)
+#    for row in reader:
+#        title = row[0].lower()
+#        title = title.replace(" ","_")
+#        title = title + "_ru"
+#        coords = [float(row[1]),float(row[2])]
+#        wrapper(coords,radius,language,dates,title)
